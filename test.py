92ebe13538ad4dcdf06aa23ad2698e06dd4a3af8
Aida Piccato <aidapiccato@gmail.com>
2015-01-31 10:38:47 -0500

    Can now get access token

--- test.py
+++ test.py
@@ -1,24 +1,18 @@
 import requests 
 import urllib2, urllib
 import base64
+import json
 # r = requests.get('https://api.github.com/events')
 
 ## Encoding consumer key and consumer secret ##
-request_token = {}
-key = "3sTAvWUnGJZA2m5zzrctNUiFs"
-secret = "78ma62jkA7d4mrm6xXTUcRzzX6F4iGaIFGpoFUSAevqUuZt4wC"
- 
-credentials = key + ":" + secret
-
-credentials = base64.b64encode(credentials)
-
-print(credentials)
-
-## Making HTTP Post requests ##
-grant_type = "client_credentials"
 headers = {}
-headers["Authorization"] = credentials
+consumer_key = "3sTAvWUnGJZA2m5zzrctNUiFs"
+consumer_secret = "78ma62jkA7d4mrm6xXTUcRzzX6F4iGaIFGpoFUSAevqUuZt4wC"
+authorization = consumer_key+":"+consumer_secret
+headers['Authorization'] = "Basic " + base64.b64encode(authorization)
 headers["Content-Type"] = "application/x-www-form-urlencoded;charset=UTF-8."
-headers["grant_type"] = "client_credentials"
 
-r = requests.post("https://api.twitter.com/oauth2/token", headers=headers)
+data = {}
+data["grant_type"] = "client_credentials"
+r = requests.post("https://api.twitter.com/oauth2/token", headers=headers, data=data)
+print(r.text)
